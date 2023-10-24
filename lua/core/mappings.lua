f6487807b8648e953cd9e3d660ea37f3a290dad2
Gennadiy Ivchshenko <gena9902@gmail.com>
2023-10-24 08:22:10 +0600

    autocomplite

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 
 -- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>')
+vim.keymap.set('n', '<leader>fe', ':Neotree float<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree focus git_status<CR>')
+vim.keymap.set('n', '<leader>fo', ':Neotree float git_status<CR>')
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,29 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ 'phaazon/hop.nvim' },
-	{ 'nvim-neo-tree/neo-tree.nvim',
-	  branch = "v2.x",
-	  dependencies = {
-		"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
-		}
-	},
-	{ 'nvim-treesitter/nvim-treesitter' },
-	{ 'williamboman/mason.nvim' },
-	{ 'williamboman/mason-lspconfig.nvim' },
-	{ 'neovim/nvim-lspconfig' },
-	{ 'edeneast/nightfox.nvim' },
-	{ 'joshdick/onedark.vim' }
+    { 'phaazon/hop.nvim' },
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = "v2.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
+        }
+    },
+    { 'nvim-treesitter/nvim-treesitter' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    { 'edeneast/nightfox.nvim' },
+    { 'joshdick/onedark.vim' },
+    { 'hrsh7th/cmp-nvim-lsp' }, { 'hrsh7th/cmp-buffer' }, { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' }, { 'hrsh7th/nvim-cmp' },
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 })
