82aa5979fc3f5b6c90e8bca6d896e1f075a25340
Gennadiy Ivashchenko <gena9902@gmail.com>
2023-11-09 22:35:18 +0600

    move from vssnip to luasnip; add icon-picker.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,51 +1,75 @@
 ---@diagnostic disable: undefined-global
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { 'phaazon/hop.nvim' },
-    {
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
-        }
-    },
-    { 'nvim-treesitter/nvim-treesitter' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    {
-        "jay-babu/mason-null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "jose-elias-alvarez/null-ls.nvim",
-        },
-        config = function()
-            require("your.null-ls.config") -- require your null-ls config here (example below)
-        end,
-    },
-    { 'neovim/nvim-lspconfig' },
-    { 'edeneast/nightfox.nvim' },
-    { 'joshdick/onedark.vim' },
-    { 'hrsh7th/cmp-nvim-lsp' }, { 'hrsh7th/cmp-buffer' }, { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' }, { 'hrsh7th/nvim-cmp' }, { 'hrsh7th/vim-vsnip' },
-    { 'hrsh7th/vim-vsnip-integ' },
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'jose-elias-alvarez/null-ls.nvim' }
+	{ "phaazon/hop.nvim" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			"s1n7ax/nvim-window-picker",
+		},
+	},
+	{ "nvim-treesitter/nvim-treesitter" },
+
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+		},
+		config = function()
+			require("plugins.nullls") -- require your null-ls config here (example below)
+		end,
+	},
+
+	{
+		"nvimtools/none-ls.nvim",
+		name = "null-ls",
+	},
+	{ "neovim/nvim-lspconfig" },
+
+	{ "edeneast/nightfox.nvim" },
+	{ "joshdick/onedark.vim" },
+
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "Gelio/cmp-natdat", config = true },
+
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(_, _)
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{ "saadparwaiz1/cmp_luasnip" },
+
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+
+	{ "stevearc/dressing.nvim" },
+	{ "ziontee113/icon-picker.nvim" },
 })
