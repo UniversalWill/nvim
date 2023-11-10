c5ffb67b3f9a72bfffc46a8a70655b2d42ff904a
Gennadiy Ivachshenko <gena9902@gmail.com>
2023-11-10 12:27:05 +0600

    add open buffers to mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
 
 -- NeoTree
-vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>')
-vim.keymap.set('n', '<leader>fe', ':Neotree float<CR>')
-vim.keymap.set('n', '<leader>o', ':Neotree focus git_status<CR>')
-vim.keymap.set('n', '<leader>fo', ':Neotree float git_status<CR>')
+vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>")
+vim.keymap.set("n", "<leader>fe", ":Neotree float<CR>")
+vim.keymap.set("n", "<leader>b", ":Neotree focus buffers<CR>")
+vim.keymap.set("n", "<leader>be", ":Neotree float buffers<CR>")
+vim.keymap.set("n", "<leader>o", ":Neotree focus git_status<CR>")
+vim.keymap.set("n", "<leader>fo", ":Neotree float git_status<CR>")
