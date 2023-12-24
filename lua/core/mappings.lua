139ff404af80538fb7f9e9cc37789d23494ac051
Gennadiy Ivashchenko <gena9902@gmail.com>
2023-12-24 23:24:58 +0600

    add ToggleTerm mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -19,3 +19,8 @@ vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 -- Tabs
 vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
+
+-- Terminal
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
+vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical size=40<CR>")
