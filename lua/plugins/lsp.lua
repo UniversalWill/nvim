97af5cff0855f2ae9c7fef6cfc4642eef1f6920c
Gennadiy Ivashchenko <gena9902@gmail.com>
2024-08-26 17:48:36 +0500

    from notebook

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,33 +8,33 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 lspconfig.pyright.setup({})
 lspconfig.lua_ls.setup({
-	capabilities = Capabilities,
-	on_attach = On_attach,
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    capabilities = Capabilities,
+    on_attach = On_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
+lspconfig.clangd.setup({})
 lspconfig.bashls.setup({})
-lspconfig.ccls.setup({})
 lspconfig.csharp_ls.setup({})
 lspconfig.docker_compose_language_service.setup({})
 lspconfig.dockerls.setup({})
@@ -50,27 +50,27 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<Leader>sa", vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set("n", "<Leader>sr", vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set("n", "<Leader>sl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, opts)
-		vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
-		-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<Leader>lf", function()
-			fmt.format({ async = true })
-		end, opts)
-	end,
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<Leader>sa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<Leader>sr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<Leader>sl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<Leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<Leader>lr", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<Leader>la", vim.lsp.buf.code_action, opts)
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<Leader>lf", function()
+            fmt.format({ async = true })
+        end, opts)
+    end,
 })
