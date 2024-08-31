6b7084b6420f13b806222d605aca3abdb2b36cf1
Gennadiy Ivashchenko <gena9902@gmail.com>
2024-08-31 08:23:02 +0500

    add Clang lsp and formatter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -64,8 +64,10 @@ require("lazy").setup({
 
     {
         "L3MON4D3/LuaSnip",
-        tag = "v2.*",
-        run = "make install_jsregexp",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
         config = function(_, _)
             require("luasnip.loaders.from_vscode").lazy_load()
         end,
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,25 @@
 local fmt = require("conform")
 
 fmt.setup({
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
 })
 
 -- python
 fmt.formatters_by_ft.python = { "ruff_format", "ruff_fix" }
+
+--c
+fmt.formatters_by_ft.c = { "clang-format" }
 
 -- charp
 fmt.formatters_by_ft.cs = { "csharpier" }
