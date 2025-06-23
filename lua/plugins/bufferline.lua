b7804f347dfd9330365184bf003c6c2a268709d5
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-06-23 10:14:28 +0500

    feat: disable offsets and change style for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,19 +2,27 @@ require("bufferline").setup({
 	options = {
 		buffer_close_icon = "",
 		mode = "buffers",
-		offsets = {
-			{
-				filetype = "neo-tree",
-				text = "Neo Tree",
-				separator = true,
-				-- padding = 1,
-			},
-		},
+		-- offsets = {
+		-- 	{
+		-- 		filetype = "neo-tree",
+		-- 		text = "Neo Tree",
+		-- 		separator = true,
+		-- 		-- padding = 1,
+		-- 	},
+		-- },
+		modified_icon = "●",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
 		diagnostics = "nvim_lsp",
 		indicator = {
-			icon = "  ", -- this should be omitted if indicator style is not 'icon'
-			style = "icon",
+			-- icon = "  ", -- this should be omitted if indicator style is not 'icon'
+			style = "none",
 		},
-		-- separator_style = "slope"
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			local icon = level:match("error") and " " or " "
+			return " " .. icon .. count
+		end,
+		always_show_bufferline = true,
+		separator_style = "thin",
 	},
 })
