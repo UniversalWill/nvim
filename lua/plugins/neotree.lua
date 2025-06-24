c2bc313d13217ade0ffdf346b0dedadce865d31a
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-06-24 12:41:55 +0500

    feat: refactor deprecate sigh_define for neotree; remove window section

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,16 +1,16 @@
 -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-
-require("neo-tree").setup({
-	window = {
-		mapping = {
-			["<s-Tab>"] = "prev_source",
-			["<Tab>"] = "next_source",
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "󰌵",
 		},
 	},
+})
+
+require("neo-tree").setup({
 	source_selector = {
 		winbar = true,
 		sources = {
