b7186bdb771cec3efb7979b8c3be392ff6cdc144
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-06-25 08:17:07 +0500

    feat: add render markdown

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -145,4 +145,12 @@ require("lazy").setup({
 	},
 
 	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 })
