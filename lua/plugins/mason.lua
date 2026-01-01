4f4175f7e63370a5f75dfc5df089a6f744833c31
Ivachshenko Gennadiy <gena9902@gmail.com>
2026-01-01 18:11:35 +0500

    feat: add icon for mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,12 @@
-require("mason").setup()
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
 -- Disable because attach two same lsp server to buffer
 -- require("mason-lspconfig").setup({
 -- 	automatic_installation = true,
