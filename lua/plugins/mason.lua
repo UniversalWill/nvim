4e6c70d69444181e910f22dc76d4cffb8b6c567c
Ivachshenko Gennadiy <gena9902@gmail.com>
2026-01-03 16:47:24 +0500

    feat: mason lsp enable automatic install

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,6 +8,6 @@ require("mason").setup({
 	},
 })
 -- Disable because attach two same lsp server to buffer
--- require("mason-lspconfig").setup({
--- 	automatic_installation = true,
--- })
+require("mason-lspconfig").setup({
+	automatic_installation = true,
+})
