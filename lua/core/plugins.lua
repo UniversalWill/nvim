bb6d69f9f3440f4d19673a3fae5cba51d18db9f8
Ivachshenko Gennadiy <gena9902@gmail.com>
2026-03-28 11:20:58 +0500

    chore: cleanup indentation and update plugin configurations

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,150 +1,150 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"antosha417/nvim-lsp-file-operations",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-neo-tree/neo-tree.nvim",
-		},
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-			{
-				"s1n7ax/nvim-window-picker", -- for open_with_window_picker keymaps
-				version = "2.*",
-				config = function()
-					require("window-picker").setup({
-						filter_rules = {
-							include_current_win = false,
-							autoselect_one = true,
-							-- filter using buffer options
-							bo = {
-								-- if the file type is one of following, the window will be ignored
-								filetype = { "neo-tree", "neo-tree-popup", "notify" },
-								-- if the buffer type is one of following, the window will be ignored
-								buftype = { "terminal", "quickfix" },
-							},
-						},
-					})
-				end,
-			},
-		},
-		lazy = false,
-	},
-	{ "nvim-treesitter/nvim-treesitter" },
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neo-tree/neo-tree.nvim",
+        },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+            {
+                "s1n7ax/nvim-window-picker", -- for open_with_window_picker keymaps
+                version = "2.*",
+                -- config = function()
+                -- 	require("window-picker").setup({
+                -- 		filter_rules = {
+                -- 			include_current_win = false,
+                -- 			autoselect_one = true,
+                -- 			-- filter using buffer options
+                -- 			bo = {
+                -- 				-- if the file type is one of following, the window will be ignored
+                -- 				filetype = { "neo-tree", "neo-tree-popup", "notify" },
+                -- 				-- if the buffer type is one of following, the window will be ignored
+                -- 				buftype = { "terminal", "quickfix" },
+                -- 			},
+                -- 		},
+                -- 	})
+                -- end,
+            },
+        },
+        lazy = false,
+    },
+    { "nvim-treesitter/nvim-treesitter" },
 
-	{ "williamboman/mason.nvim", build = ":MasonUpdate" },
-	{ "williamboman/mason-lspconfig.nvim" },
+    { "williamboman/mason.nvim",          build = ":MasonUpdate" },
+    { "williamboman/mason-lspconfig.nvim" },
 
-	{ "neovim/nvim-lspconfig" },
+    { "neovim/nvim-lspconfig" },
 
-	{ "stevearc/conform.nvim" },
+    { "stevearc/conform.nvim" },
 
-	{ "edeneast/nightfox.nvim" },
-	{ "joshdick/onedark.vim" },
+    { "edeneast/nightfox.nvim" },
+    { "joshdick/onedark.vim" },
 
-	{
-		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "*",
-	},
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "*",
+    },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		},
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+    },
 
-	{ "stevearc/dressing.nvim" },
+    { "stevearc/dressing.nvim" },
 
-	{ "ziontee113/icon-picker.nvim" },
+    { "ziontee113/icon-picker.nvim" },
 
-	{ "folke/trouble.nvim" },
+    { "folke/trouble.nvim" },
 
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
 
-	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+    { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 
-	{ "terrortylor/nvim-comment" },
+    { "terrortylor/nvim-comment" },
 
-	{
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+    {
+        "nvimdev/dashboard-nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	{ "lewis6991/gitsigns.nvim" },
+    { "lewis6991/gitsigns.nvim" },
 
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"linrongbin16/lsp-progress.nvim",
-		},
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "linrongbin16/lsp-progress.nvim",
+        },
+    },
 
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+    { "akinsho/toggleterm.nvim", version = "*", config = true },
 
-	-- {
-	--     "folke/which-key.nvim",
-	--     event = "VeryLazy",
-	--     init = function()
-	--         vim.o.timeout = true
-	--         vim.o.timeoutlen = 300
-	--     end,
-	--     dependencies = { 'echasnovski/mini.nvim', version = false },
-	-- },
+    -- {
+    --     "folke/which-key.nvim",
+    --     event = "VeryLazy",
+    --     init = function()
+    --         vim.o.timeout = true
+    --         vim.o.timeoutlen = 300
+    --     end,
+    --     dependencies = { 'echasnovski/mini.nvim', version = false },
+    -- },
 
-	{
-		"linrongbin16/lsp-progress.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lsp-progress").setup({})
-		end,
-	},
+    {
+        "linrongbin16/lsp-progress.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lsp-progress").setup({})
+        end,
+    },
 
-	{ "wakatime/vim-wakatime", lazy = false },
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-	},
-	{
-		url = "https://codeberg.org/andyg/leap.nvim",
-	},
+    { "wakatime/vim-wakatime", lazy = false },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    },
+    {
+        url = "https://codeberg.org/andyg/leap.nvim",
+    },
 })
